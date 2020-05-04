48f00f90e39d245cb806ef658aa53dfd5ad4d85d
Marcus Vinícius Ibraim <mvibraim@gmail.com>
2020-05-04 07:05:59 -0300

    Return JSON response for route not found

--- src/api/index.js
+++ src/api/index.js
@@ -5,4 +5,13 @@ const apiRouter = new Router()
 
 apiRouter.use('/resellers', resellersRouter)
 
+apiRouter.get('*', function (req, res) {
+  res
+    .status(404)
+    .json({
+      message: "route not found",
+      status: 404
+    });
+});
+
 export default apiRouter
