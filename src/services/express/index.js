690b4532dce90540ae73a412040f7ce9d1a9a8fa
Marcus Vinícius Ibraim <mvibraim@gmail.com>
2020-05-10 03:32:24 -0300

    feat: Default error as JSON

--- src/services/express/index.js
+++ src/services/express/index.js
@@ -5,15 +5,20 @@ import helmet from "helmet";
 import morgan from "morgan";
 import compression from "compression";
 
+let jsonErrorHandler = async (err, req, res, next) => {
+  res.status(err.status).send({ error: err });
+};
+
 let app = (apiRoot, routes) => {
   let app = express();
 
   app.use(helmet());
-  app.use(bodyParser.json());
   app.use(cors());
   app.use(morgan("combined"));
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(jsonErrorHandler);
   app.use(apiRoot, routes);
 
   return app;
